f6b62cf7e092bfa9c41b9e7eb578cb42a76f3cc4
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-04-19 14:35:15 +0200

    changes to server js, due to error logs on heroku.

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-const express = require('express');
-const path = require('path');
-const compression = require('compression');
+import express from 'express';
+import path from 'path';
+import compression from 'compression';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const BASE_URL = process.env.VITE_BASE_URL;
-
 app.use(compression());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'dist'))); 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html')); 
-  });
+    app.use(express.static(path.resolve(__dirname, 'dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+    });
 }
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-

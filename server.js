a4edf2c15ef2ce1611fc6837ffb55393fd4b0788
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-04-19 14:45:46 +0200

    updates to package and server. tested locally before push.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import compression from 'compression';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;

3f75eab9efa33c9c4177b237730903f97c48ffed
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-05-19 15:30:20 +0200

    Refactor package-lock.json, package.json, server.js, and vite.config.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const PORT = process.env.PORT || 3000;
 app.use(compression());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-    });
+  app.use(express.static(path.resolve(__dirname, 'dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  });
 }
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser' 
+    outDir: 'dist',
+    minify: 'terser'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BASE_URL,
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   }
 });

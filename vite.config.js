fca484101b79179a23437a0ec0c3a07127279e58
Moonchichiii <matgus1983@gmail.com>
2024-05-27 15:24:26 +0200

    config update.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser' 
+    outDir: 'dist',
+    minify: 'terser'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BASE_URL,
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   }
 });
